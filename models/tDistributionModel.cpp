9009d189b303ec6df79abd46a4d2bcb0cbf14550
arshita.gupta <arshitagupta27@gmail.com>
2019-09-20 08:09:04 -0400

    updated tDistribution

--- models/tDistributionModel.cpp
+++ models/tDistributionModel.cpp
@@ -32,7 +32,7 @@ public:
 
 		// Initial covar
 		Mat covarMat = Mat::eye(100, 100, CV_64FC1);
-		covarMat = covarMat*(rand() % 255) * 1000;
+		covarMat = covarMat*(rand() % 255) * 100;
 
 		return make_pair(meansMat.clone(), covarMat.clone());
 		}
@@ -49,14 +49,11 @@ public:
 		    double arr[dataset[i].size()];
 		    std::copy(dataset[i].begin(), dataset[i].end(), arr);
 			cv::Mat single_dataset(1, 100, CV_64FC1, arr);
-			single_dataset = single_dataset.reshape(1,1);
-			Mat d = (single_dataset - mean)*(covar.inv());
+			Mat d = (single_dataset - mean)*(covar.inv()); // (1x100)
 			Mat d1 = d*((single_dataset - mean).t());
-			expectation_h.at<double>(i,0) = (v+D)/(v+d1.at<double>(i,0));
+			expectation_h.at<double>(i,0) = (v+D)/(v+d1.at<double>(0,0));
 			expectation_logh.at<double>(i,0) = boost::math::digamma(((v/2) + (D/2)))
-											- exp((v/2)+ (d1.at<double>(i,0)/2));
-//			expectation_h[i]=(v+D)/(v+d1[i])
-//			expectation_logh[i] = scipy.special.digamma(((v/2) + (D/2)), out=None) - np.log((v/2) + (d1[i]/2))
+											- log((v/2)+ (d1.at<double>(i,0)/2));
 		}
 		return make_pair(expectation_h.clone(), expectation_logh.clone());
 	}
@@ -82,23 +79,23 @@ public:
 		for (int i=0; i<dataset.at(0).size(); i++){
 			vector<double> a;
 			cv::reduce(y_temp(Range::all(), Range(i,i+1)), a,0,CV_REDUCE_SUM);
-			new_mean.at<double>(0,i) = a.at(0)/y_sum;
+			new_mean.at<double>(0,i) = a[0]/y_sum;
 		}
 
-		cv::Mat covar_temp( dataset.at(0).size(),dataset.size(), CV_64FC1);
-		for (int i=0; i<dataset.at(0).size(); i++){
+		Mat covar_temp( dataset.size(), dataset.at(0).size()* dataset.at(0).size(), CV_64FC1);
+		for (int i=0; i<dataset.size(); i++){
 			double arr[dataset[i].size()];
 			std::copy(dataset[i].begin(), dataset[i].end(), arr);
 			cv::Mat single_dataset(1, 100, CV_64FC1, arr);
-			single_dataset = single_dataset.reshape(1,1);
-			Mat d = ((single_dataset - new_mean).t())* (expectation_h.at<double>(i,0)*(single_dataset - new_mean));
-			covar_temp.row(i) = d.reshape(1,1);
+			cv::Mat d = ((single_dataset - new_mean).t())* (expectation_h.at<double>(i,0)*(single_dataset - new_mean));
+			d.convertTo(d, CV_64FC1);
+			d.reshape(1,1).row(0).copyTo(covar_temp.row(0));
 		}
-		cv::Mat new_covar(1, dataset.at(0).size(), CV_64FC1);
-		for (int i=0; i<dataset.at(0).size(); i++){
+		cv::Mat new_covar(1, dataset.at(0).size()*dataset.at(0).size(), CV_64FC1);
+		for (int i=0; i<dataset.at(0).size()*dataset.at(0).size(); i++){
 			vector<double> a;
-			cv::reduce(covar_temp(Range(i,i+1), Range::all()), a,0,CV_REDUCE_SUM);
-			new_covar.at<double>(0,i) = a.at(0)/y_sum;
+			cv::reduce(covar_temp(Range::all(), Range(i,i+1)), a,0,CV_REDUCE_SUM);
+			new_covar.at<double>(0,i) = a[0]/y_sum;
 		}
 		new_covar = new_covar.reshape(1,100);
 
@@ -178,6 +175,11 @@ public:
 			double D_face = 100;
 			double v_nonface = 300;
 			double D_nonface = 100;
+			Mat expectation_h_face;
+			Mat expectation_logh_face;
+			Mat expectation_h_nonface;
+			Mat expectation_logh_nonface;
+
 			std::cout<<"Parameters initialized"<<endl;
 			for (int i=0; i<n_iterations; i++){
 				std::cout<<"\niteration no: "<<i<<endl;
@@ -187,34 +189,33 @@ public:
 												covar_face,
 												v_face,
 												D_face);
-				std::cout<<"E-step complete for Face "<<endl;
-				Mat expectation_h_face = estep_face.first;
-				Mat expectation_logh_face = estep_face.second;
+				expectation_h_face = estep_face.first;
+				expectation_logh_face = estep_face.second;
 				pair<Mat, Mat> mstep_face = Mstep(train_face,
 												expectation_h_face,
 												expectation_logh_face,
 												v_face);
-				Mat mean_face = mstep_face.first;
-				Mat covar_face = mstep_face.second;
+				std::cout<<"E-step complete for Face "<<endl;
+				mean_face = mstep_face.first;
+				covar_face = mstep_face.second;
 				v_face= cal_v(v_face,
 							expectation_h_face,
 							expectation_logh_face);
 				std::cout<<"M-step complete for Face "<<endl;
-
 				pair<Mat, Mat> estep_nonface = Estep(train_nonface,
 													mean_nonface,
 													covar_nonface,
 													v_nonface,
 													D_nonface);
+				expectation_h_nonface = estep_nonface.first;
+				expectation_logh_nonface = estep_nonface.second;
 				std::cout<<"E-step complete for Non-Face "<<endl;
-				Mat expectation_h_nonface = estep_nonface.first;
-				Mat expectation_logh_nonface = estep_nonface.second;
 				pair<Mat, Mat> mstep_nonface = Mstep(train_nonface,
 												expectation_h_nonface,
 												expectation_logh_nonface,
 												v_nonface);
-				Mat mean_nonface = mstep_nonface.first;
-				Mat covar_nonface = mstep_nonface.second;
+				mean_nonface = mstep_nonface.first;
+				covar_nonface = mstep_nonface.second;
 				v_nonface = cal_v(v_nonface,
 								expectation_h_nonface,
 								expectation_logh_nonface);
